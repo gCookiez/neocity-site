4a8b74997937fce68f9e58d7c9f45038e99ce737
Marcus Gajo <musou_saber@proton.me>
2026-04-18 10:46:37 +0800

    Adjusted nav buttons
    - adjusted hitbox size on buttons
    
    -assigned hover events to hitbox on css
    
    Adjusted shadows

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('Hello!')
+
+
+const button = document.querySelectorAll('.hover-container');
+
+button.forEach(i => {
+    const btnTxtContent = i.querySelector('.hover-item').textContent.trim();
+    const btnHitBox = i.querySelector('.hover-hitbox');
+
+    btnHitBox.addEventListener('click', () => {
+        console.log(btnTxtContent);
+    })
+})
